83ffe03f0eaaf998c5b4c2576a7327bfd4108a00
Lance Johnson <lancejohnson728@gmail.com>
2024-04-17 11:01:27 -0500

    Add delivery information parsing

--- app.py
+++ app.py
@@ -1,22 +1,38 @@
 import os
+from datetime import datetime
 
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 
-def transform_data(data):
-    return data['user'].upper()
+def parse_delivery_information(tracking_data):
+    delivery_information = {}
+    delivery_tracking_data = tracking_data['tracking_details'][-1]
+    delivery_information['delivery_city'] = delivery_tracking_data['tracking_location']['city'].title()
+    delivery_information['delivery_state'] = delivery_tracking_data['tracking_location']['state'].upper()
+
+    delivery_datetime = datetime.strptime(delivery_tracking_data['datetime'], '%Y-%m-%dT%H:%M:%SZ')
+    delivery_information['delivery_date'] = delivery_datetime.date()
+    delivery_information['delivery_date_readable'] = delivery_datetime.strftime('%a %-m/%-d')
+
+    return delivery_information
 
 
 @app.route('/delivery_status', methods=['POST'])
 def webhook():
-    data = request.json
-    print("Received webhook data:", data)
-    transformed_data = transform_data(data)
-    return jsonify({"status": "success", "message": "Data received", "transformed_data": transformed_data}), 200
+    tracking_data = request.json
+    print("Received webhook data:", tracking_data)
+    carrier = tracking_data['carrier']
+    tracking_number = tracking_data['tracking_code']
+    if tracking_data['status'] == "delivered":
+        delivery_information = parse_delivery_information(tracking_data)
+        status = "delivered"
+    else:
+        delivery_information = "in transit"
+    return jsonify({"status": "success", "carrier": carrier, "tracking_number": tracking_number, "delivery_status": status, "delivery_information": delivery_information}), 200
 
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
-    app.run(debug=False, host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0', port=port)
