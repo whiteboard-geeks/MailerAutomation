a0a1cdd0e57a8991c20c03d382e5c1f874fdc8a1
Marc Püls <mpuels@gmail.com>
2025-11-11 11:30:16 +0100

    rm celery code for handler /easypost/delivery_status

--- config.py
+++ config.py
@@ -9,11 +9,6 @@ def _str_to_bool(value: str | None, *, default: bool = False) -> bool:
 
 env_type = os.getenv("ENV_TYPE", "development")
 
-USE_TEMPORAL_FOR_EASYPOST_DELIVERY_STATUS = _str_to_bool(
-    os.getenv("USE_TEMPORAL_FOR_EASYPOST_DELIVERY_STATUS"), default=False
-)
-
 print("=== ENVIRONMENT INFO ===")
 print(f"ENV_TYPE: {env_type}")
-print(f"USE_TEMPORAL_FOR_EASYPOST_DELIVERY_STATUS: {USE_TEMPORAL_FOR_EASYPOST_DELIVERY_STATUS}")
 print("=== END ENVIRONMENT INFO ===")
